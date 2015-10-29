cd09351ee6fb5b1fbb41a948a8bbb0fcf6b89ded
Jean-Francois Champagne <champagne.jeanfrancois@gmail.com>
2015-10-28 21:33:13 -0400

    Adding parent to a level 2 taxonomy name

--- PrototypeLog410/TaxonomyForm.Designer.cs
+++ PrototypeLog410/TaxonomyForm.Designer.cs
@@ -57,6 +57,7 @@
             this.label1.Size = new System.Drawing.Size(55, 24);
             this.label1.TabIndex = 0;
             this.label1.Text = "Coral";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // label2
             // 
@@ -68,6 +69,7 @@
             this.label2.Size = new System.Drawing.Size(46, 24);
             this.label2.TabIndex = 1;
             this.label2.Text = "Fish";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
             // label3
             // 
@@ -79,6 +81,7 @@
             this.label3.Size = new System.Drawing.Size(57, 24);
             this.label3.TabIndex = 2;
             this.label3.Text = "Other";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
             // 
             // label4
             // 
@@ -90,6 +93,7 @@
             this.label4.Size = new System.Drawing.Size(47, 24);
             this.label4.TabIndex = 3;
             this.label4.Text = "Pink";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
             // 
             // label5
             // 
@@ -101,6 +105,7 @@
             this.label5.Size = new System.Drawing.Size(45, 24);
             this.label5.TabIndex = 4;
             this.label5.Text = "Red";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
             // 
             // label6
             // 
@@ -112,6 +117,7 @@
             this.label6.Size = new System.Drawing.Size(48, 24);
             this.label6.TabIndex = 5;
             this.label6.Text = "Blue";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
             // 
             // label7
             // 
@@ -123,6 +129,7 @@
             this.label7.Size = new System.Drawing.Size(62, 24);
             this.label7.TabIndex = 6;
             this.label7.Text = "Green";
+            this.label7.Click += new System.EventHandler(this.label7_Click);
             // 
             // label8
             // 
@@ -134,6 +141,7 @@
             this.label8.Size = new System.Drawing.Size(47, 24);
             this.label8.TabIndex = 7;
             this.label8.Text = "Pike";
+            this.label8.Click += new System.EventHandler(this.label8_Click);
             // 
             // label9
             // 
@@ -145,6 +153,7 @@
             this.label9.Size = new System.Drawing.Size(59, 24);
             this.label9.TabIndex = 8;
             this.label9.Text = "Shark";
+            this.label9.Click += new System.EventHandler(this.label9_Click);
             // 
             // label10
             // 
@@ -156,6 +165,7 @@
             this.label10.Size = new System.Drawing.Size(73, 24);
             this.label10.TabIndex = 9;
             this.label10.Text = "Dolphin";
+            this.label10.Click += new System.EventHandler(this.label10_Click);
             // 
             // label11
             // 
@@ -167,6 +177,7 @@
             this.label11.Size = new System.Drawing.Size(54, 24);
             this.label11.TabIndex = 10;
             this.label11.Text = "Sand";
+            this.label11.Click += new System.EventHandler(this.label11_Click);
             // 
             // label12
             // 
@@ -178,6 +189,7 @@
             this.label12.Size = new System.Drawing.Size(48, 24);
             this.label12.TabIndex = 11;
             this.label12.Text = "Alga";
+            this.label12.Click += new System.EventHandler(this.label12_Click);
             // 
             // label13
             // 
@@ -189,6 +201,7 @@
             this.label13.Size = new System.Drawing.Size(53, 24);
             this.label13.TabIndex = 12;
             this.label13.Text = "Rock";
+            this.label13.Click += new System.EventHandler(this.label13_Click);
             // 
             // hScrollBar1
             // 
--- PrototypeLog410/TaxonomyForm.cs
+++ PrototypeLog410/TaxonomyForm.cs
@@ -13,10 +13,13 @@ namespace PrototypeLog410
     public partial class TaxonomyForm : Form
     {
         private List<TaxonomyAssociation> taxonomyAssociations;
+        private TaxonomyTransformation currTaxonomyTransformation;
 
         public TaxonomyForm()
         {
             InitializeComponent();
+
+            currTaxonomyTransformation = null;
 
             taxonomyAssociations = new List<TaxonomyAssociation>();
 
@@ -49,18 +52,135 @@ namespace PrototypeLog410
 
         private void addParentButton_Click(object sender, EventArgs e)
         {
-
+            resetLabelBackColors();
+            currTaxonomyTransformation = new TaxonomyTransformation { isAddition = true, level1Label = null, level2Label = null };
         }
 
         private void removeParentButton_Click(object sender, EventArgs e)
         {
+            
+        }
 
+        private void resetLabelBackColors()
+        {
+            label4.BackColor = Control.DefaultBackColor;
+            label5.BackColor = Control.DefaultBackColor;
+            label6.BackColor = Control.DefaultBackColor;
+            label7.BackColor = Control.DefaultBackColor;
+            label8.BackColor = Control.DefaultBackColor;
+            label9.BackColor = Control.DefaultBackColor;
+            label10.BackColor = Control.DefaultBackColor;
+            label11.BackColor = Control.DefaultBackColor;
+            label12.BackColor = Control.DefaultBackColor;
+            label13.BackColor = Control.DefaultBackColor;
         }
 
         public class TaxonomyAssociation
         {
             public Label level1Label { get; set; }
             public Label level2Label { get; set; }
+        }
+
+        public class TaxonomyTransformation
+        {
+            public bool isAddition { get; set; }
+            public Label level1Label { get; set; }
+            public Label level2Label { get; set; }
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+            onLevel2LabelClicked(label4);
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+            onLevel2LabelClicked(label5);
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+            onLevel2LabelClicked(label6);
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+            onLevel2LabelClicked(label7);
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+            onLevel2LabelClicked(label8);
+        }
+
+        private void label9_Click(object sender, EventArgs e)
+        {
+            onLevel2LabelClicked(label9);
+        }
+
+        private void label10_Click(object sender, EventArgs e)
+        {
+            onLevel2LabelClicked(label10);
+        }
+
+        private void label11_Click(object sender, EventArgs e)
+        {
+            onLevel2LabelClicked(label11);
+        }
+
+        private void label12_Click(object sender, EventArgs e)
+        {
+            onLevel2LabelClicked(label12);
+        }
+
+        private void label13_Click(object sender, EventArgs e)
+        {
+            onLevel2LabelClicked(label13);
+        }
+
+        private void onLevel2LabelClicked(Label label)
+        {
+            if(currTaxonomyTransformation == null || currTaxonomyTransformation.level2Label != null)
+            {
+                return;
+            }
+
+            label.BackColor = Color.Lime;
+            currTaxonomyTransformation.level2Label = label;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            onLevel1LabelClicked(label1);
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            onLevel1LabelClicked(label2);
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+            onLevel1LabelClicked(label3);
+        }
+
+        private void onLevel1LabelClicked(Label label)
+        {
+            if (currTaxonomyTransformation == null || currTaxonomyTransformation.level2Label == null)
+            {
+                return;
+            }
+
+            if (currTaxonomyTransformation.isAddition)
+            {
+                taxonomyAssociations.Add(new TaxonomyAssociation { level1Label = label, level2Label = currTaxonomyTransformation.level2Label });
+            }
+
+            currTaxonomyTransformation = null;
+            resetLabelBackColors();
+
+            Invalidate();
+            Update();
         }
     }
 }
